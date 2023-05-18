b631a3c308d51aeffd12a86eaf9a2e38a4f36229
MUKHTER <mohammadmukhter@gmail.com>
2023-05-19 00:22:42 +0600

    toy data post done

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toyBikroy').collection('toys');
+   
+
+    // toy insert data
+    app.post("/toys", async(req, res)=> {
+        const toyData = req.body;
+        console.log(toyData);
+        const result = await toyCollection.insertOne(toyData)
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
